c718353a61cc8cdf366a1194581f61b25f1264a3
Aimee Shen <aimee_shen@trend.com.tw>
2016-07-11 16:41:41 +0800

    Question component unit test.

---
+++ web/test/components/feedback/Question.js
@@ -0,0 +1,30 @@
+import DomMock from '../../helpers/dom-mock';
+import jsdom from 'mocha-jsdom';
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Question from '../../../src/components/Question';
+
+DomMock('<html><body></body></html>');
+
+describe('Testing Question Component', () => {
+    jsdom({ skipWindowCheck: true });
+
+    it('should contain correct question id and text', () => {
+        const content = TestUtils.renderIntoDocument(
+            <Question id={2} text={'Question 2'} />
+        );
+        const question = TestUtils.findRenderedDOMComponentWithClass(content, 'ut-question');
+
+        expect(question.textContent).toEqual('Q2. Question 2');
+    });
+
+    it('should contain *required tag', () => {
+        const content = TestUtils.renderIntoDocument(
+            <Question required={true} />
+        );
+        const requiredTag = TestUtils.findRenderedDOMComponentWithClass(content, 'ut-required');
+
+        expect(requiredTag.textContent).toEqual('*required');
+    });
+});
