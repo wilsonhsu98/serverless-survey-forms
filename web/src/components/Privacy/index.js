accb80aa5c9209e71d5fd031f8a8beaa33a8b04b
Aimee Shen <aimee_shen@trend.com.tw>
2016-07-11 18:22:49 +0800

    Privacy component unit test.

--- web/src/components/Privacy/index.js
+++ web/src/components/Privacy/index.js
@@ -7,26 +7,31 @@ import styles from './style.css';
 
 import React, { PropTypes } from 'react';
 import PureComponent from 'react-pure-render/component';
-import $ from 'jquery';
+import classNames from 'classnames';
 
 class Privacy extends PureComponent {
-
-    componentDidMount() {
-        $(this.refs.root).localize();
-    }
-
-    componentDidUpdate() {
-        $(this.refs.root).localize();
-    }
 
     render() {
         const { item } = this.props;
         return (
             <div ref="root" className="question">
-                <div className={styles.question}>{item.label}</div>
+                <div
+                    className={classNames({
+                        [`${styles.question}`]: true,
+                        'ut-label': true
+                    })}
+                >{item.label}
+                </div>
                 <div className={styles.terms}>
                     <input type="checkbox" />
-                    <div className={styles.desc}>{item.terms}</div>
+                    <div
+                        className={classNames({
+                            [`${styles.desc}`]: true,
+                            'ut-terms': true
+                        })}
+                    >
+                    {item.terms}
+                    </div>
                     <input type="text" placeholder={item.input} />
                 </div>
             </div>
