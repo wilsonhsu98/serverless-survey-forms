8a1eee4a7739974ab7bcac7df913a197c932d102
Chiou Chu <enjoychu@gmail.com>
2016-07-13 18:51:37 +0800

    Edit panel style and click button event

--- web/portal/src/components/EditPanel/EditPage/index.js
+++ web/portal/src/components/EditPanel/EditPage/index.js
@@ -11,19 +11,8 @@ class EditPage extends PureComponent {
     constructor() {
         super();
 
-        this._mouseClickEvent = this._mouseClickEvent.bind(this);
-        this._panelClickEvent = this._panelClickEvent.bind(this);
+        this._btnClickEvent = this._btnClickEvent.bind(this);
         this._handleChangeEvent = this._handleChangeEvent.bind(this);
-    }
-
-    componentDidMount() {
-        window.addEventListener('click', this._mouseClickEvent);
-        $('#editPanel').on('click', this._panelClickEvent);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('click', this._mouseClickEvent);
-        $('#editPanel').off('click', this._panelClickEvent);
     }
 
     render() {
@@ -31,45 +20,40 @@ class EditPage extends PureComponent {
         const page = questions[editPage - 1];
 
         return (
-            <div id="editPanel" className="editpanel">
-                <div>Edit Page</div>
-                <div className={styles.item}>
-                    Page #{page.page}:&nbsp;
-                    <input
-                        type="text"
-                        value={page.description}
-                        onChange={this._handleChangeEvent}
-                    />
-                </div>
+            <div className="modalEditPanel">
+                <div id="editPanel" className="editpanel">
+                    <div>Edit Page</div>
+                    <div className={styles.item}>
+                        Page #{page.page}:&nbsp;
+                        <input
+                            type="text"
+                            value={page.description}
+                            onChange={this._handleChangeEvent}
+                        />
+                    </div>
 
-                <div className="bottom">
-                    <button
-                        data-type="save"
-                        className="actionBtn"
-                    >
-                        Save
-                    </button>
-                    <button
-                        data-type="cancel"
-                        className="actionBtn"
-                    >
-                        Cancel
-                    </button>
+                    <div className="bottom">
+                        <button
+                            data-type="save"
+                            className="actionBtn"
+                            onClick={this._btnClickEvent}
+                        >
+                            Save
+                        </button>
+                        <button
+                            data-type="cancel"
+                            className="actionBtn"
+                            onClick={this._btnClickEvent}
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </div>
             </div>
         );
     }
 
-    _mouseClickEvent(e) {
-        const { editPageActions } = this.props;
-        if (e.target.id !== 'editPanel') {
-            editPageActions.stopEditPage();
-        }
-    }
-
-    _panelClickEvent(e) {
-        e.stopPropagation();
-
+    _btnClickEvent(e) {
         if (e.target.getAttribute('data-type') === 'cancel') {
             const { editPageActions } = this.props;
             editPageActions.stopEditPage();
