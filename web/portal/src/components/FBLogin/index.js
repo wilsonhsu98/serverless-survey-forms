7f296bc3b037fe4dcbec5e8a4a9b243c403e83ca
Chiou Chu <enjoychu@gmail.com>
2016-07-18 15:36:29 +0800

    Login page component

--- web/portal/src/components/FBLogin/index.js
+++ web/portal/src/components/FBLogin/index.js
@@ -1,6 +1,6 @@
 
 // CSS
-// import styles from './style.css';
+import styles from './style.css';
 
 import React from 'react';
 import PureComponent from 'react-pure-render/component';
@@ -17,11 +17,16 @@ class FBLogin extends PureComponent {
 
     render() {
         return (
-            <div ref="root">
-                <h1>The survey form you ever need</h1>
-                <div>Cras quis nulla commodo, aliquam lectus sed, blandit augue. Crasullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus atsemper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sedlacinia sem elementum quis. Aliquam consectetur, eros etvulputate euismod, nunc leo tempor lacus, ac rhoncus neque erosnec lacus. Cras lobortis molestie faucibus.</div>
+            <div ref="root" className={styles.wrap}>
+                <div className={styles.wording}>
+                    <div className={styles.title}>The survey form you ever need</div>
+                    <div className={styles.description}>Cras quis nulla commodo, aliquam lectus sed, blandit augue. Crasullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus atsemper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sedlacinia sem elementum quis. Aliquam consectetur, eros etvulputate euismod, nunc leo tempor lacus, ac rhoncus neque erosnec lacus. Cras lobortis molestie faucibus.</div>
 
-                <button onClick={this._onClickFBBtn}>Log in with your Facebook</button>
+                    <button className={styles.grantBtn} onClick={this._onClickFBBtn}>Log in with your Facebook</button>
+                </div>
+
+                <div className={styles.photo}>
+                </div>
             </div>
         );
     }
