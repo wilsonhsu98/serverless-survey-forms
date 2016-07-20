074009c1438dc04502d8945ac993555777d4c67e
Chiou Chu <enjoychu@gmail.com>
2016-07-20 12:13:29 +0800

    Revert "Set orderPage reducer to be pure"
    This reverts commit b62b376f471d2fe856f9127039939dd01b28f422.

--- web/portal/src/reducers/orderPage.js
+++ web/portal/src/reducers/orderPage.js
@@ -4,8 +4,14 @@ import * as types from '../constants/ActionTypes';
 export default function orderPage(state = [], action) {
     switch (action.type) {
     case types.SET_ORDERPAGE:
-    case types.EXCHANGE_ORDERPAGE:
         return [...action.orderPage];
+    case types.EXCHANGE_ORDERPAGE:
+        let originP = [...state];
+        const { bfIdx:bfPageIdx, afIdx:afPageIdx } = action;
+        const movePage = originP[bfPageIdx];
+        originP.splice(bfPageIdx, 1);
+        originP.splice(afPageIdx, 0, movePage);
+        return [...originP];
     default:
         return state;
     }
