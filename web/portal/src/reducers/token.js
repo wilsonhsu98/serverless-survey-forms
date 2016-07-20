3350680299eab499327ff40397396b2fdfc90ddc
Chiou Chu <enjoychu@gmail.com>
2016-07-20 11:03:08 +0800

    Set token reducer to be pure

--- web/portal/src/reducers/token.js
+++ web/portal/src/reducers/token.js
@@ -4,7 +4,6 @@ import * as types from '../constants/ActionTypes';
 export default function token(state = '', action) {
     switch (action.type) {
     case types.SET_TOKEN:
-        window.localStorage["QustomPortalTK"] = action.token;
         return action.token;
     default:
         return state;
