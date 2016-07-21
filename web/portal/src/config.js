bf94b21a0ce751eff698c1a59d596d49d5acaea7
Chiou Chu <enjoychu@gmail.com>
2016-07-21 19:01:20 +0800

    Adjust FB login flow

--- web/portal/src/config.js
+++ web/portal/src/config.js
@@ -2,7 +2,7 @@
 
 // setup server url
 const Config = {
-    baseURL: 'https://r2c5wmub95.execute-api.ap-northeast-1.amazonaws.com/devjim/'
+    baseURL: 'https://r2c5wmub95.execute-api.ap-northeast-1.amazonaws.com/devjim'
 };
 
 export default Config;
