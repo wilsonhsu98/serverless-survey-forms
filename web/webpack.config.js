f2512965aeaaf0da8c25f9e2d6654e2e5d007920
Chiou Chu <enjoychu@gmail.com>
2016-07-21 16:13:37 +0800

    Adjust scripts and node server for develop and deploy

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,12 +1,107 @@
 var path = require('path');
 var webpack = require('webpack');
-var baseConfig = require('./webpack.config.base');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var folder = process.env.NODE_FOLDER;
 
-var webpackConfig = Object.create(baseConfig);
-webpackConfig.entry = [path.resolve(__dirname, 'src/index.js')];
-webpackConfig.output = {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "survey.js"
+// Postcss plugins
+var postcssImport = require('postcss-import');
+var postcssNested = require('postcss-nested');
+var autoprefixer = require('autoprefixer');
+
+var webpackConfig = {
+    entry: path.resolve(__dirname, folder + '/src/entry.js'),
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, '../client/dist/' + folder)
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js?$/,
+                loader: 'eslint-loader',
+                include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'feedback')],
+                exclude: /node_modules/
+            }
+        ],
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', "css-loader?modules!postcss-loader"),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            JSON: path.resolve(__dirname, 'json')
+        }
+    },
+    postcss: function plugins(bundler) {
+        return [
+            postcssImport({
+                addDependencyTo: bundler
+            }),
+            postcssNested,
+            autoprefixer({ browsers: 'last 2 version' })
+        ];
+    }
 };
+
+// Plugins for different environment
+if (process.env.NODE_ENV === 'production') {
+    webpackConfig.plugins = [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new ExtractTextPlugin("styles.css", { allChunks: true }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                unused: true,
+                dead_code: true,
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Qustom',
+            template: path.resolve(__dirname, 'assets/html/index.html'),
+            filename: 'index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        })
+    ];
+} else {
+    webpackConfig.devtool = "source-map";
+    webpackConfig.plugins = [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin("styles.css", { allChunks: true }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"development"'
+            }
+        })
+    ];
+}
 
 module.exports = webpackConfig;
