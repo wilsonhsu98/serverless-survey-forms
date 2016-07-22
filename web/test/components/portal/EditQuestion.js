6327cfa5b31f2aef6eaffdb15a1133389fb5d591
Chiou Chu <enjoychu@gmail.com>
2016-07-22 13:45:21 +0800

    Adjust portal editQuestion unit test

--- web/test/components/portal/EditQuestion.js
+++ web/test/components/portal/EditQuestion.js
@@ -8,37 +8,44 @@ import EditQuestion from '../../../portal/src/components/EditPanel/EditQuestion'
 
 DomMock('<html><body></body></html>');
 
-const fakeData = {
-    id: '1AN2AL0F9BNA7A',
-    type: 'rating',
-    label: 'Testing question text',
-    data: [
-        { value: 1, label: 'Very dissatisfied' },
-        { value: 2, label: 'Very satisfied' }
-    ],
-    input: 'Tell us the reason why you choose this answe',
-    required: false
-};
-
 describe('[Portal] Testing EditQuestion Component', () => {
     jsdom({ skipWindowCheck: true });
 
-    it('show question editor: question label and advance', () => {
-        const props = {
-            editQuestion: fakeData,
-            editQuestionActions: () => {},
-            questionsActions: () => {}
-        };
+    const fakeData = {
+        id: '1AN2AL0F9BNA7A',
+        type: 'rating',
+        label: 'Testing question text',
+        data: [
+            { value: 1, label: 'Very dissatisfied' },
+            { value: 2, label: 'Very satisfied' }
+        ],
+        input: 'Tell us the reason why you choose this answe',
+        required: false
+    };
+    const props = {
+        editQuestion: fakeData,
+        editQuestionActions: () => {},
+        questionsActions: () => {}
+    };
 
-        const EditQuestionContext = wrapInTestContext(EditQuestion);
-        const contentRoot = TestUtils.renderIntoDocument(<EditQuestionContext {...props} />);
-        // const backend = contentRoot.getManager().getBackend();
+    const EditQuestionContext = wrapInTestContext(EditQuestion);
+    const contentRoot = TestUtils.renderIntoDocument(<EditQuestionContext {...props} />);
+
+    it('show question editor: question label', () => {
         const textarea = TestUtils.scryRenderedDOMComponentsWithClass(contentRoot, 'ut-editQuestion');
         // Expect  component
         expect(textarea[0].textContent).toEqual(fakeData.label);
+    });
 
+    it('show question editor: question advance', () => {
         const div = TestUtils.scryRenderedDOMComponentsWithClass(contentRoot, 'ut-advance');
         // Expect one component
         expect(div.length).toEqual(1);
+    });
+
+    it('show question editor: question advance checkbox', () => {
+        const chk = TestUtils.scryRenderedDOMComponentsWithClass(contentRoot, 'ut-chk');
+        // Expect one check component
+        expect(chk[0].checked).toEqual(true);
     });
 });
