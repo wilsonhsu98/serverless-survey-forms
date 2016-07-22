085edc8d846a019402feb3821a41bd8fb8f4c25b
Chiou Chu <enjoychu@gmail.com>
2016-07-22 12:20:00 +0800

    Portal editQuestion unit test

---
+++ web/test/helpers/dnd-test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TestBackend from 'react-dnd-test-backend';
+import { DragDropContext } from 'react-dnd';
+
+/**
+ * Wraps a component into a DragDropContext that uses the TestBackend.
+ */
+export function wrapInTestContext(DecoratedComponent) {
+    return DragDropContext(TestBackend)(
+        class TestContextContainer extends Component {
+            render() {
+                return <DecoratedComponent {...this.props} />;
+            }
+        }
+    );
+}
