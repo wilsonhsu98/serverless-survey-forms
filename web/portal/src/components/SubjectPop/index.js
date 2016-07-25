b2446f92c010425d1be40a31fd550950dd3c7ef0
Chiou Chu <enjoychu@gmail.com>
2016-07-25 17:29:41 +0800

    Adopt style for edit subject popup

--- web/portal/src/components/SubjectPop/index.js
+++ web/portal/src/components/SubjectPop/index.js
@@ -17,46 +17,62 @@ class SubjectPop extends PureComponent {
         const { subject } = this.props;
         return (
             <div className={styles.popup}>
-                <div className={styles.content}>
-                    <div className={styles.title}>What would you like to name this survey?</div>
-                    <input
-                        id="subject"
-                        className={styles.input}
-                        type="text"
-                        defaultValue={subject}
-                        placeholder="Subject Name"
-                    />
+                <div className={styles.wrap}>
+                    <div
+                        data-type="cancel"
+                        className={`${styles.close} close`}
+                        onClick={this._btnClickEvent}
+                    >X</div>
+                    <div className={styles.content}>
+                        <div className={styles.title}>What would you like to name this survey?</div>
+                        <input
+                            id="subject"
+                            className={`${styles.input} input input--medium`}
+                            type="text"
+                            defaultValue={subject}
+                            placeholder="Subject Name"
+                            onChange={this._handleInput}
+                        />
+                        <div id="msg" className={`${styles.input__msg} input__msg`}></div>
 
-                    <div className={`bottom ${styles.buttom}`}>
-                        <button
-                            data-type="save"
-                            className="actionBtn"
-                            onClick={this._btnClickEvent}
-                        >
-                            Save
-                        </button>
-                        <button
-                            data-type="cancel"
-                            className="actionBtn"
-                            onClick={this._btnClickEvent}
-                        >
-                            Cancel
-                        </button>
+                        <div className={`bottom ${styles.bottom}`}>
+                            <button
+                                data-type="save"
+                                className={`${styles.button} button button--medium button--red`}
+                                onClick={this._btnClickEvent}
+                            >
+                                Save
+                            </button>
+                            <button
+                                data-type="cancel"
+                                className={`${styles.button} button button--medium`}
+                                onClick={this._btnClickEvent}
+                            >
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 
+    _handleInput() {
+        const msg = document.getElementById('msg');
+        if (msg.innerHTML.length) msg.innerHTML = '';
+    }
+
     _btnClickEvent(e) {
         const { surveyID, editSubjectActions, subjectActions } = this.props;
+        const msg = document.getElementById('msg');
+        msg.innerHTML = '';
 
         if (e.target.getAttribute('data-type') === 'cancel') {
             editSubjectActions.openEdit(false);
         } else if (e.target.getAttribute('data-type') === 'save') {
             const subject = document.getElementById('subject').value;
             if (subject === '') {
-                alert('Please fill the subject');
+                msg.innerHTML = 'Please fill the subject';
             } else {
                 if (!surveyID) {
                     subjectActions.saveSubject(subject);
