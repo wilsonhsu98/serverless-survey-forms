146c686a5603f01a279760d4bd05d3b562cfc179
Chiou Chu <enjoychu@gmail.com>
2016-08-02 18:14:33 +0800

    Fix login style postision in large size

--- web/portal/src/components/FBLogin/index.js
+++ web/portal/src/components/FBLogin/index.js
@@ -19,29 +19,31 @@ class FBLogin extends PureComponent {
         return (
             <div ref="root" className={styles.wrap}>
                 <div className={styles.content_bg}></div>
-                <div className={styles.content}>
-                    <div className={styles.title}>The survey form you ever need</div>
-                    <div className={styles.description}>
-                        Cras quis nulla commodo, aliquam lectus sed, blandit augue.
-                        Crasullamcorper bibendum bibendum.
-                        Duis tincidunt urna non pretium porta.
-                        Nam condimentum vitae ligula vel ornare. Phasellus atsemper turpis.
-                        Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci.
-                        Donec id dignissim nunc. Donec elit ante, eleifend a dolor et,
-                        venenatis facilisis dolor. In feugiat orci odio,
-                        sedlacinia sem elementum quis.
-                        Aliquam consectetur, eros etvulputate euismod,
-                        nunc leo tempor lacus, ac rhoncus neque erosnec lacus.
-                        Cras lobortis molestie faucibus.
-                    </div>
-                    <div className={styles.grantBtn}>
-                        <Button
-                            string="Log in with your Facebook"
-                            i18nKey={false}
-                            color="ruby"
-                            size="medium"
-                            onClick={this._onClickFBBtn}
-                        />
+                <div className={styles.container}>
+                    <div className={styles.content}>
+                        <div className={styles.title}>The survey form you ever need</div>
+                        <div className={styles.description}>
+                            Cras quis nulla commodo, aliquam lectus sed, blandit augue.
+                            Crasullamcorper bibendum bibendum.
+                            Duis tincidunt urna non pretium porta.
+                            Nam condimentum vitae ligula vel ornare. Phasellus atsemper turpis.
+                            Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci.
+                            Donec id dignissim nunc. Donec elit ante, eleifend a dolor et,
+                            venenatis facilisis dolor. In feugiat orci odio,
+                            sedlacinia sem elementum quis.
+                            Aliquam consectetur, eros etvulputate euismod,
+                            nunc leo tempor lacus, ac rhoncus neque erosnec lacus.
+                            Cras lobortis molestie faucibus.
+                        </div>
+                        <div className={styles.grantBtn}>
+                            <Button
+                                string="Log in with your Facebook"
+                                i18nKey={false}
+                                color="ruby"
+                                size="medium"
+                                onClick={this._onClickFBBtn}
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className={styles.photo}></div>
