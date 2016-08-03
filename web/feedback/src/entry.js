d47e4db8c66c6f8b18aa1cd97e214c672f2a4d2b
Aimee Shen <aimee_shen@trend.com.tw>
2016-08-03 14:46:20 +0800

    Prepare postMessage env for receicing info from client.

--- web/feedback/src/entry.js
+++ web/feedback/src/entry.js
@@ -6,17 +6,44 @@ import ReactDOM from 'react-dom';
 import Qustom from '../../src/index';
 
 class App extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.receiveClientMessage = this.receiveClientMessage.bind(this);
+        this.state = {
+            clientData: ''
+        };
+    }
+
+    componentDidMount() {
+        // Create IE + others compatible event handler
+        const eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent';
+        const eventer = window[eventMethod];
+        const messageEvent = eventMethod === 'attachEvent' ? 'onmessage' : 'message';
+        eventer(messageEvent, (e) => this.receiveClientMessage(e), false);
+    }
+
     render() {
+        // TODO: get surveyID/accountID from Client
         return (
             <div>
                 <Qustom
                     accountid="context.authorizer.principalId"
                     surveyid="759e7930-3219-11e6-b8fc-ed3df7fb1eab"
                     localize_path="../../assets/L10N"
-                    type="preview"
+                    type="embedded"
                 />
             </div>
         );
+    }
+
+    receiveClientMessage(e) {
+        // TODO: event origin detection
+        // if (e.origin !== 'https://qustomURL') return;
+        console.log('Message received from Client!:  ', e.data);
+        this.setState({
+            clientData: e.data
+        });
     }
 }
 
