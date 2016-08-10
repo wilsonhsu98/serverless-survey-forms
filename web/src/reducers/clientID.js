4c1a63f804bf75cbdddaafd85230c1a8d88f1a12
Aimee Shen <aimee_shen@trend.com.tw>
2016-08-10 13:44:52 +0800

    Fixed reducer function name.

--- web/src/reducers/clientID.js
+++ web/src/reducers/clientID.js
@@ -1,6 +1,6 @@
 import * as types from '../constants/ActionTypes';
 
-export default function paging(state = '', action) {
+export default function clientID(state = '', action) {
     switch (action.type) {
     case types.SAVE_CLIENT_ID:
         return action.clientID;
