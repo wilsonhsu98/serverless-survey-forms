c8d91fbf8307873c82e4b70e34521c3b34e39527
Chiou Chu <enjoychu@gmail.com>
2016-08-16 18:51:20 +0800

    Fix unit test in controlBtn

--- web/test/components/portal/ControlBtn.js
+++ web/test/components/portal/ControlBtn.js
@@ -23,32 +23,16 @@ describe('[Portal] Testing ControlBtn Component', () => {
             <ControlBtn {...props} />
         );
         const btn = TestUtils.scryRenderedComponentsWithType(content, IconButton);
-        expect(btn.length).toEqual(4);
+        expect(btn.length).toEqual(1);
     });
 
-    it('survey list control button: if no selectedSurveys, buttons disable will be true', () => {
-        const content = TestUtils.renderIntoDocument(
-            <ControlBtn {...props} />
-        );
-        const btn = TestUtils.scryRenderedComponentsWithType(content, IconButton);
-        expect(btn[0].props.disabled).toBe(false);
-        expect(btn[1].props.disabled).toBe(true);
-        // TODOS: wait feature ready
-        // expect(btn[2].props.disabled).toBe(true);
-        expect(btn[3].props.disabled).toBe(true);
-    });
-
-    it('survey list control button: if one selectedSurveys, buttons disable will be false', () => {
+    it('survey list control button: if one selectedSurveys, buttons will show', () => {
         props.selectedSurveys = '2222-3333-4444-0000';
 
         const content = TestUtils.renderIntoDocument(
             <ControlBtn {...props} />
         );
         const btn = TestUtils.scryRenderedComponentsWithType(content, IconButton);
-        expect(btn[0].props.disabled).toBe(false);
-        expect(btn[1].props.disabled).toBe(false);
-        // TODOS: wait feature ready
-        // expect(btn[2].props.disabled).toBe(false);
-        expect(btn[3].props.disabled).toBe(false);
+        expect(btn.length).toEqual(4);
     });
 });
