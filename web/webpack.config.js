1eb67020826b69756e652b3596228b7773377afc
Chiou Chu <enjoychu@gmail.com>
2017-10-26 11:46:47 +0800

    Increase inline images size to 10000

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -80,7 +80,7 @@ var webpackConfig = {
                     }
                 }
             },
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=10000' },
             { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=65000&mimetype=application/font-woff" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=65000&mimetype=application/octet-stream]" },
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=65000&mimetype=application/vnd.ms-fontobject" },
